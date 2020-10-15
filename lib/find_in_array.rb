11444a7cb6226a1c5bc89de5abaac23a363fa850
samumar43 <samumar43@gmail.com>
2020-10-15 15:36:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   while count < array.size do
     if array[count] == value_to_find
       found_value_index = count
-    end
+    end 
     count += 1 
   end 
 end
